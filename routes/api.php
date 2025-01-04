7ab34953c0d7fd1388b57da0cd60700378f89244
allan81 <rjaycondiman81@gmail.com>
2025-01-05 03:28:55 +0800

    API Resource compare code below

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,10 @@ use App\Http\Controllers\PersonalInformationController;
 
 Route::middleware('api')->group(function () {
     Route::apiResource('personal-information', PersonalInformationController::class);
+    // API Resource is Equivalent to Like this code Below
+    // Route::get('personal-information', [PersonalInformationController::class, 'index']);
+    // Route::post('personal-information', [PersonalInformationController::class, 'store']);
+    // Route::get('personal-information/{id}', [PersonalInformationController::class, 'show']);
+    // Route::put('personal-information/{id}', [PersonalInformationController::class, 'update']);
+    // Route::delete('personal-information/{id}', [PersonalInformationController::class, 'destroy']);
 });
